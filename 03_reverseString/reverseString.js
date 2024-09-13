17a10cd0db3c497bf9b84e137bec03a7c67b19f1
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-13 13:57:21 -0400

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let stringReversed = "";
+  for (char of string) {
+    stringReversed = char + stringReversed;
+  }
+  return stringReversed;
 };
 
 // Do not edit below this line
