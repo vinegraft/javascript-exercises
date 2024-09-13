97d7940b9ba43620c0c4b46d8b6e13c40344e205
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-13 16:26:09 -0400

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let numStart = 0;
+  let numEnd = 0;
+  let numTotal = 0;
+  for (arg in arguments) {
+    if (arguments[arg] < 0) {
+      return "ERROR";
+    }
+    if (arguments[arg] != Math.floor(arguments[arg])) {
+      return "ERROR";
+    }
+    if (!Number.isInteger(arguments[arg])) {
+      return "ERROR";
+    }
+  }
+  if (a <= b) {
+    numStart = a;
+    numEnd = b;
+  } else {
+    numStart = b;
+    numEnd = a;
+  }
+  for (i = numStart; i <= numEnd; i++) {
+    numTotal += i;
+  }
+  return numTotal;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
