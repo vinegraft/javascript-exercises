29eb46e7da6362a60305c5fc832730f2029f54cc
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-13 15:36:34 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removed) {
+  let keepGoing = true;
+  while (keepGoing === true) {
+    keepGoing = false;
+    for (i in array) {
+      for (j in removed) {
+        if (array[i] === removed[j]) {
+          array.splice(i, 1);
+          keepGoing = true;
+        }
+      }
+    }
+  }
+  return array;
 };
-
+console.log(
+  `Array with items removed: ${removeFromArray([1, 2, 3, 3, 4], 3, 4)}`
+);
 // Do not edit below this line
 module.exports = removeFromArray;
